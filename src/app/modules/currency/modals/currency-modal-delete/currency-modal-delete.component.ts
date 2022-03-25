ff6767f14c4153a92560de6ee4a418d21f5979ea
kevin jimenez <orlando.kj24@gmail.com>
2022-03-25 00:16:17 -0500

    doing: formularios y ui

---
+++ src/app/modules/currency/modals/currency-modal-delete/currency-modal-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-modal-delete',
+  templateUrl: './currency-modal-delete.component.html',
+  styleUrls: ['./currency-modal-delete.component.scss']
+})
+export class CurrencyModalDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/currency/modals/currency-modal-edit-create/currency-modal-edit-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-modal-edit-create',
+  templateUrl: './currency-modal-edit-create.component.html',
+  styleUrls: ['./currency-modal-edit-create.component.scss']
+})
+export class CurrencyModalEditCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
